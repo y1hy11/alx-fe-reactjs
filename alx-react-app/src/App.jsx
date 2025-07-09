e69351214ac98505a1771c0befbf1609b0074df2
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-09 20:14:46 +0100

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './components/Header.jsx'
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <WelcomeMessage /> 
+      <MainContent />
+      <WelcomeMessage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
