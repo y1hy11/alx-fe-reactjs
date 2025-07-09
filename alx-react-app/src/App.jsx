cb70bb835a295a8c667b19647c50dfc5ab251648
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-09 20:18:37 +0100

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage.jsx";
 import Header from "./components/Header.jsx";
 import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainContent />
       <WelcomeMessage />
+      <Footer />
     </>
   );
 }
