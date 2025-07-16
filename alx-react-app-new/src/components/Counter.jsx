4cffca5fe94706f2972cbbb35d51df3bf860a00a
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-16 20:53:38 +0100

    update

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
 function Counter() {
+    const [count, setCount] = useState(0);
 
-};
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    const reset = () => setCount(0);
+}
 
 export default Counter;
