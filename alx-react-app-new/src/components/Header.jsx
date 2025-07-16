acb0b217d4e05a4c71edc0bf3e11c5966fbeff45
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-16 02:47:51 +0100

    update

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <header>
+        <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
             <h1>My Favorite Cities</h1>
         </header>
     );
