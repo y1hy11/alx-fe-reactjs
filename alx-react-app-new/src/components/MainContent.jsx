99029913b220c63c652e10cb12341dfd3081e6f0
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-16 20:32:06 +0100

    update

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <p style={{ fontWeight: 'bold' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
