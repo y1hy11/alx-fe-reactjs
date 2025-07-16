1a1eee4ce0df54d59bc834e58fe1de847c67568c
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-16 20:57:40 +0100

    update

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,11 +4,13 @@ import Header from "./components/Header.jsx";
 import UserProfile from "./components/UserProfile.jsx";
 import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
+import Counter from "./components/Counter.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Counter />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
