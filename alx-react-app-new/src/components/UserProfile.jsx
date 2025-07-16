258cd4cf6d1bc9fa4d386f418d3ddcc8d7e86adb
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-16 02:51:41 +0100

    update

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 const UserProfile = (props) => {
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2 style={{ color: 'blue' }}>{props.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
     );
