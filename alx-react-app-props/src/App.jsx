f12aa456934678ff80793a48b892468ac5d92374
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-17 22:27:23 +0100

    update

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -6,11 +6,17 @@ import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
+  const userData = {
+    name: "Alice",
+    email: "alice@example.com",
+    age: "25",
+    bio: "Loves hiking and photography",
+  };
   return (
     <>
       <Header />
       <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name={userData.name} email={userData.email} age={userData.age} bio={userData.bio} />
       <WelcomeMessage />
       <Footer />
     </>
