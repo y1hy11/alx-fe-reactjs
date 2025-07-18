088be4d31cdf8b3702dab3a24f7d547c2d50cb31
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-18 21:41:52 +0100

    update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
