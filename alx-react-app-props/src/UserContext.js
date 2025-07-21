e939afe59dd30f2ea73551c569c0756f778b446e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-21 01:30:09 +0100

    update

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -2,6 +2,4 @@ import React from 'react';
 
 const UserContext = React.createContext();
 
-export const UserProvider = UserContext.Provider;
-export const UserConsumer = UserContext.Consumer;
 export default UserContext;
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,3 +1,5 @@
+
+
 const UserProfile = (props) => {
     return (
         <div >
