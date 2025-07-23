84e582d6cce8b9185a7968bc366994b7fdb881bb
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-23 20:30:37 +0100

    feat: render AddRecipeForm and RecipeList components in App

--- recipe-sharing-app/react/src/App.jsx
+++ recipe-sharing-app/react/src/App.jsx
@@ -1,9 +1,14 @@
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <AddRecipeForm />
+      <RecipeList />
     </>
   )
 }
