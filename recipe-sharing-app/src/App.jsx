6cab12851d099f62b9836aa636500100ce7cc3f5
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-25 16:43:51 +0100

    feat: implement routing in App component for recipe navigation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-      <RecipeDetails />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
