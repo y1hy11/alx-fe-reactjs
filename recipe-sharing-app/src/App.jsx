80aa930603fbfa23bf9b2ea8ffc3204a32d31b90
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-25 16:42:04 +0100

    feat: add RecipeDetails component to App for improved recipe viewing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,6 @@
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <AddRecipeForm />
       <RecipeList />
+      <RecipeDetails />
     </>
   )
 }
