022e38de081cafd42eafac36b49f97f832f035e1
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-26 19:28:58 +0100

    add FavoritesList component and update routing in App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
+import FavoritesList from './components/FavoritesList'
 import './App.css'
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<RecipeList />} />
         <Route path="/add" element={<AddRecipeForm />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </Router>
   )
