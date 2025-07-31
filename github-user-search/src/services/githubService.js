cce36f4e9d8c4beaf0e106f17a6663137b4fa058
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-07-31 22:52:54 +0100

    update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,9 +2,19 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const searchUsers = async (username) => {
+export const searchUsers = async (username, location, minRepos) => {
   try {
-    const response = await axios.get(`${BASE_URL}/search/users?q=${username}`);
+    let query = username;
+    
+    if (location) {
+      query += `+location:${location}`;
+    }
+    
+    if (minRepos) {
+      query += `+repos:>=${minRepos}`;
+    }
+    
+    const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
     return response.data;
   } catch (error) {
     throw new Error(`Error searching users: ${error.message}`);
