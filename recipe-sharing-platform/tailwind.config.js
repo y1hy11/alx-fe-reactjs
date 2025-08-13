bdfb200b746881d0bc52b5932b51cb4fd9c5a22b
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-08-13 20:41:05 +0100

    update tailwind config

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: false, // You can set it to 'media' or 'class' if needed
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
